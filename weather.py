70a0a6100f9ab586f76960fc27fb8e5f8ec1ea10
Robert <lum.robert@gmail.com>
2015-06-29 01:12:12 -0400

    refactor(weather): converted to class; will contain all methods related to weather api; including returning back formatted data

--- weather.py
+++ weather.py
@@ -5,9 +5,15 @@ BASE_URL = "http://api.wunderground.com/api/{0}/conditions/q/{1}/{2}.json"
 STATE = "NY"
 LOCATION = "New_York"
 
-def make_request():
-	response = requests.get(BASE_URL.format(API_KEY, STATE, LOCATION))
-	print response._content
+class Weather():
 
-if __name__ == "__main__":
-	make_request()
+	def __init__(self):
+		self.make_request()
+
+	def make_request(self):
+		response = requests.get(BASE_URL.format(API_KEY, STATE, LOCATION))
+		print response._content
+		# return response._content
+
+	def format_hourly(self):
+		pass
