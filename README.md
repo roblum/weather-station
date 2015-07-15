6cd463b94e824fa7eda3dcbea5a1a371bc848ec9
Rob Lum <lum.robert@gmail.com>
2015-07-15 17:30:52 -0400

    feat(service): setting up flask socket and test index.html page to communicate

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Motion Activated Weather Station
 
 Using opencv and motion detection, retrieve the latest hourly weather conditions
+
+How to run socket:
+gunicorn -k flask_sockets.worker service:app
---
+++ service.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sockets import Sockets
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+@sockets.route('/weather_station')
+def weather_station(ws):
+    while True:
+        message = ws.receive()
+
+        ws.send(message)
